560f33391b483e36967bde36c4b2e887c40bc1d9
AshieTran <trandinhquy080107@gmail.com>
2025-12-28 00:10:18 +0700

    Update1: Finished function to print matrix and get the members

--- src/Tensor.cpp
+++ src/Tensor.cpp
@@ -1,13 +1,32 @@
 #include"MiniTensor/Tensor.h"
-
+#include<stdexcept> // EXCEPTION HANDLING
 namespace MiniTensor{
     //Default values
-    Tensor::Tensor(int rows, int cols): m_rows(rows), m_cols(cols){
-        m_data.resize(rows*cols, 0.0f);
-    }
-
-    void Tensor::print() const{
+    Tensor::Tensor(int rows, int cols){
+        m_data.resize(m_rows*m_cols, 0.0);
+    };
+    const double& Tensor::at(int r, int c) const{
+        if(r >= m_rows || c >= m_cols){
+            throw std::out_of_range("INDEX OUT OF BOUNDS!!");
+        }
+        return m_data[r*m_cols + c];
+    };
+    double& Tensor::at(int r, int c){
+        if(r >= m_rows || c >= m_cols){
+            throw std::out_of_range("INDEX OUT OF BOUNDS!!");
+        }
+        return m_data[r*m_cols + c];
+    };
+    void Tensor::print_shape() const{
         std::cout << "Tensor Shape: (" << m_rows << "," << m_cols << ")" << std::endl;
+    }
+    void Tensor::print_matrix() const{
+        for(size_t i = 0; i < m_rows*m_cols; i+= m_cols){
+            for(size_t j = i; j < m_cols; j++){
+                std::cout << m_data[j];
+            }
+            std::cout << std::endl;
+        }
     }
 
 }
