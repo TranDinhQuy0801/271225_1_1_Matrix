8329a3de7fdb03ea5acf1e2051fc165fb08c637a
AshieTran <trandinhquy080107@gmail.com>
2025-12-29 00:17:12 +0700

    Finished the random_matrix function

--- src/Tensor.cpp
+++ src/Tensor.cpp
@@ -1,11 +1,23 @@
 #include"MiniTensor/Tensor.h"
 #include<stdexcept> // EXCEPTION HANDLING
+#include<random>
 namespace MiniTensor{
     //Default values
     Tensor::Tensor(int rows, int cols)
         : m_rows(rows), m_cols(cols){ // Initializer lists
         m_data.resize(rows*cols, 0.0);
     };
+    void Tensor::random_matrix(double mean, double stddev){
+        static std::random_device rd; // create static var -> initialize once -> optimize
+        static std::mt19937 gen(rd());
+
+        std::normal_distribution<double> dist(mean, stddev); // Define the distribution
+        for(size_t i = 0; i < m_rows; i++){
+            for(size_t j = 0; j < m_cols; j++){
+                m_data[j + m_cols*i] = dist(gen);
+            }
+        }
+    }
     const double& Tensor::at(int r, int c) const{
         if(r >= m_rows || c >= m_cols){
             throw std::out_of_range("INDEX OUT OF BOUNDS!!");
